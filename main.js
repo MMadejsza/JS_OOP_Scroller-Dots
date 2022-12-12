6ce613d1c5d77d8c3e153f4a339108df442a744b
MMadejsza <maciej.madejsza@gmail.com>
2022-12-12 18:20:53 +0000

    keyboard support

--- main.js
+++ main.js
@@ -6,4 +6,14 @@ document.addEventListener('DOMContentLoaded', function () {
 	// document.addEventListener('swipeRight', () => console.log('R'));
 	document.addEventListener('swipeUp', () => scroller.scroll(1));
 	document.addEventListener('swipeDown', () => scroller.scroll(-1));
+	document.addEventListener('keydown', (event) => {
+		switch (event.keyCode) {
+			case 40:
+				return scroller.scroll(1);
+			case 38:
+				return scroller.scroll(-1);
+			default:
+				return;
+		}
+	});
 });
