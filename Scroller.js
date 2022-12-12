711b39ee92ddc90e3cc15e17945e0bd5dc41b388
MMadejsza <maciej.madejsza@gmail.com>
2022-12-12 15:45:04 +0000

    added class Scroller instead of functions

---
+++ Scroller.js
@@ -0,0 +1,56 @@
+class Scroller {
+	constructor(rootSelector) {
+		//- getting DOM elements
+		const rootElement = document.querySelector(rootSelector);
+		this.sections = document.querySelectorAll('section'); //- nodeList!
+		// this.currentSectionIndex = 0; //- variable to hold section position - index 0 in nodeList
+		const currentSectionIndex = [...this.sections].findIndex(
+			(element) => this.isScrolledIntoView,
+		); //- browser saves last scrolled paragraph so we have to localize it and assign proper one
+		this.currentSectionIndex = currentSectionIndex < 0 ? 0 : currentSectionIndex;
+		this.isThrottled = false; //- to prevent to much scrolling with scrollIntoView
+		this.isScrolledIntoView(this.sections[0]);
+	}
+
+	isScrolledIntoView(el) {
+		const rect = el.getBoundingClientRect();
+		const top = rect.top;
+		const bottom = rect.bottom;
+		const isVisible = top >= 0 && bottom <= window.innerHeight;
+		return isVisible;
+	}
+
+	listenScroll = (event) => {
+		//- arrow function to bind this
+		if (this.isThrottled) return;
+		this.isThrottled = true;
+		setTimeout(() => {
+			//- is asynchronic so we don't have to wait till the code is fully read
+			this.isThrottled = false;
+		}, 1000);
+
+		//- on scroll
+		const direction = event.wheelDelta < 0 ? 1 : -1; //- catching direction and conditional assignment
+		this.scroll(direction);
+	};
+	scroll = (direction) => {
+		if (direction === 1) {
+			//- forward / down the page
+			const isLastSection = this.currentSectionIndex === this.sections.length - 1; //- because length is counted from 1 and index from 0.
+			//- Assigned boolean value
+			if (isLastSection) return; //- if first or last section - we stop the function
+		} else if (direction === -1) {
+			const firstSection = this.currentSectionIndex === 0;
+			if (firstSection) return;
+		}
+		//# on wheel:
+		this.currentSectionIndex += direction; //- because we used at line 7 conditional assignment -> direction is -1/1 so we can just add it without extra "IFs".
+		this.scrollToCurrentSection();
+	};
+	scrollToCurrentSection = () => {
+		this.sections[this.currentSectionIndex].scrollIntoView({
+			behavior: 'smooth',
+			block: 'start',
+		});
+	};
+}
--- main.js
+++ main.js
@@ -1,37 +1,7 @@
 document.addEventListener('DOMContentLoaded', function () {
-	//- getting DOM elements
-	const rootElement = document.querySelector('#root');
-	const sections = document.querySelectorAll('section'); //- nodeList!
-	let currentSectionIndex = 0; //- variable to hold section position - index 0 in nodeList
-	let isThrottled = false; //- to prevent to much scrolling with scrollIntoView
+	const scroller = new Scroller('#root');
+
 	document.addEventListener('mousewheel', function (event) {
-		if (isThrottled) return;
-		isThrottled = true;
-		setTimeout(() => {
-			//- is asynchronic so we don't have to wait till the code is fully read
-			isThrottled = false;
-		}, 750);
-
-		//- on scroll
-		const direction = event.wheelDelta < 0 ? 1 : -1; //- catching direction and conditional assignment
-		scroll(direction);
+		scroller.listenScroll(event);
 	});
-
-	function scroll(direction) {
-		if (direction === 1) {
-			//- forward / down the page
-			const isLastSection = currentSectionIndex === sections.length - 1; //- because length is counted from 1 and index from 0.
-			//- Assigned boolean value
-			if (isLastSection) return; //@ if first or last section - we stop the function
-		} else if (direction === -1) {
-			const firstSection = currentSectionIndex === 0;
-			if (firstSection) return;
-		}
-		//# on wheel:
-		currentSectionIndex += direction; //- because we used at line 7 conditional assignment -> direction is -1/1 so we can just add it without extra "IFs".
-		scrollToCurrentSection();
-	}
-	function scrollToCurrentSection() {
-		sections[currentSectionIndex].scrollIntoView({behavior: 'smooth', block: 'start'});
-	}
 });
